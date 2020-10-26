5ba5dae62a5a7f02425823544e1165a345f58f09
denis-skripnik <scadens@yandex.ru>
2020-10-26 15:09:13 +0700

    Change query to link in type === in_link

--- js_modules/api.js
+++ js_modules/api.js
@@ -18,7 +18,7 @@ if (type === 'full_search' && page && query) {
     query = query.toLowerCase().trim();
     let data = await ldb.unFullQuerySearchResults(query, page);
     res.send(data);
-} else if (type === 'in_link' && query) {
+} else if (type === 'in_link' && link) {
     let data = await ldb.findInLink(link);
     res.send(data);
 }
